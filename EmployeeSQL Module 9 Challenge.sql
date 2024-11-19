32180ae26fe73f8d431406bd17af83122de7ba07
AzaniaHH <azaniahoodhosten@gmail.com>
2024-11-18 21:07:25 -0600

    Add files via upload

--- EmployeeSQL Module 9 Challenge.sql
+++ EmployeeSQL Module 9 Challenge.sql
@@ -1,3 +1,45 @@
+CREATE TABLE departments (
+    dept_no CHAR(4) PRIMARY KEY,
+    dept_name VARCHAR(40) NOT NULL
+);
+
+CREATE TABLE employees (
+    emp_no INT PRIMARY KEY,
+    emp_title_id CHAR(5) NOT NULL,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    sex CHAR(1) CHECK (sex IN ('M', 'F')),
+    hire_date DATE NOT NULL
+);
+
+CREATE TABLE salaries (
+    emp_no INT PRIMARY KEY,
+    salary INT NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no) ON DELETE CASCADE
+);
+
+CREATE TABLE titles (
+    title_id CHAR(5) PRIMARY KEY,
+    title VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE dept_emp (
+    emp_no INT,
+    dept_no CHAR(4),
+    PRIMARY KEY (emp_no, dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no) ON DELETE CASCADE,
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no) ON DELETE CASCADE
+);
+
+CREATE TABLE dept_manager (
+    dept_no CHAR(4),
+    emp_no INT,
+    PRIMARY KEY (dept_no, emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no) ON DELETE CASCADE,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no) ON DELETE CASCADE
+);
+
 SELECT *
 FROM departments;
 
